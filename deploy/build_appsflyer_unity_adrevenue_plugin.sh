085f59de45dda4eb05d3d640c8ea7fa76d18b9ff
Andrii Hahan <andrii.h@appsflyer.com>
2021-06-24 17:46:35 +0300

    update plugin to support appsflyer 6.3.0, mopub 5.17.0

--- deploy/build_appsflyer_unity_adrevenue_plugin.sh
+++ deploy/build_appsflyer_unity_adrevenue_plugin.sh
@@ -4,15 +4,15 @@ echo "Start Build for appsflyer-unity-adrevenue-plugin.unitypackage"
 
 DEPLOY_PATH=outputs
 UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
-PACKAGE_NAME="appsflyer-unity-adrevenue-plugin-6.2.2.unitypackage"
+PACKAGE_NAME="appsflyer-unity-adrevenue-plugin-6.3.0.unitypackage"
 mkdir -p $DEPLOY_PATH
 
 
 # Build the .unitypackage
-/Applications/Unity/Hub/Editor/2019.1.8f1/Unity.app/Contents/MacOS/Unity \
+/Applications/Unity/Hub/Editor/2020.3.12f1/Unity.app/Contents/MacOS/Unity \
 -gvh_disable \
 -batchmode \
--importPackage external-dependency-manager-1.2.144.unitypackage \
+-importPackage external-dependency-manager-1.2.165.unitypackage \
 -nographics \
 -logFile create_unity_core.log \
 -projectPath $PWD/../ \
@@ -20,7 +20,7 @@ mkdir -p $DEPLOY_PATH
 Assets \
 $PWD/$DEPLOY_PATH/$PACKAGE_NAME \
 -quit \
-&& echo "package exported successfully to outputs/appsflyer-unity-adrevenue-plugin-6.2.2.unitypackage" \
+&& echo "package exported successfully to outputs/appsflyer-unity-adrevenue-plugin-6.3.3.unitypackage" \
 || echo "Failed to export package. See create_unity_core.log for more info."
 
 
