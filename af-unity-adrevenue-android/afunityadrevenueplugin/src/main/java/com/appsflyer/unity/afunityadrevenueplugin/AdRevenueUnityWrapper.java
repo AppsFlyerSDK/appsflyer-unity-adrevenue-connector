6e1db0704c66ef524370b2ffb75e9438074730e6
Margot <margot.guetta@appsflyer.com>
2022-01-12 13:02:50 +0200

    RD-75258
    add api android

--- af-unity-adrevenue-android/afunityadrevenueplugin/src/main/java/com/appsflyer/unity/afunityadrevenueplugin/AdRevenueUnityWrapper.java
+++ af-unity-adrevenue-android/afunityadrevenueplugin/src/main/java/com/appsflyer/unity/afunityadrevenueplugin/AdRevenueUnityWrapper.java
@@ -3,12 +3,17 @@ package com.appsflyer.unity.afunityadrevenueplugin;
 import android.app.Application;
 import com.appsflyer.adrevenue.AppsFlyerAdRevenue;
 import com.appsflyer.adrevenue.adnetworks.AppsFlyerAdRevenueWrapperType;
+import com.appsflyer.adrevenue.adnetworks.generic.MediationNetwork;
+
+import java.util.Currency;
+import java.util.Map;
 
 public class AdRevenueUnityWrapper {
 
     /**
      * Start - currently we only support MoPub
      * In the future we will support more AppsFlyerAdRevenueWrapperTypes
+     *
      * @param application
      */
     public static void start(Application application) {
@@ -18,4 +23,16 @@ public class AdRevenueUnityWrapper {
         AppsFlyerAdRevenue.moPubWrapper().recordImpressionData();
     }
 
+    public static void startGeneric(Application application) {
+        AppsFlyerAdRevenue.initialize(new AppsFlyerAdRevenue.Builder(application)
+                .build());
+    }
+
+    public static void logAdRevenue(String monetizationNetwork,
+                                    int mediationNetwork,
+                                    Currency eventRevenueCurrency,
+                                    Double eventRevenue,
+                                    Map<String, String> nonMandatory) {
+        AppsFlyerAdRevenue.logAdRevenue(monetizationNetwork, MediationNetwork.values()[mediationNetwork], eventRevenueCurrency, eventRevenue, nonMandatory);
+    }
 }
